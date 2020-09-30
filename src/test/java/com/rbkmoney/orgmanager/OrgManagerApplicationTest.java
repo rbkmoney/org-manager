e335ab4b56cacb5560cf1c97f7f223bf5c99ec79
a.romanov <a.romanov@rbkmoney.com>
2020-09-30 12:24:34 +0300

    Initial commit

---
+++ src/test/java/com/rbkmoney/orgmanager/OrgManagerApplicationTest.java
@@ -0,0 +1,16 @@
+package com.rbkmoney.orgmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = OrgManagerApplication.class)
+public class OrgManagerApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
