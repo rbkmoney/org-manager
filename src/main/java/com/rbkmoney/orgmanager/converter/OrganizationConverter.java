b0457d842605e7569ea431012093d0c4e93a7e96
a.romanov <a.romanov@rbkmoney.com>
2020-10-06 11:44:02 +0300

    BJ-993: Fix todos in OrganizationConverter

--- src/main/java/com/rbkmoney/orgmanager/converter/OrganizationConverter.java
+++ src/main/java/com/rbkmoney/orgmanager/converter/OrganizationConverter.java
@@ -1,8 +1,8 @@
 package com.rbkmoney.orgmanager.converter;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rbkmoney.orgmanager.entity.OrganizationEntity;
+import com.rbkmoney.orgmanager.util.JsonMapper;
 import com.rbkmoney.swag.organizations.model.Organization;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -11,22 +11,21 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
-import java.util.Map;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class OrganizationConverter {
 
-    private final ObjectMapper objectMapper;
+    private final JsonMapper jsonMapper;
 
-    @SneakyThrows(JsonProcessingException.class)
     public OrganizationEntity toEntity(Organization organization) {
         return OrganizationEntity.builder()
-                .id(organization.getId()) // TODO [a.romanov]: generated?
+                .id(UUID.randomUUID().toString())
                 .createdAt(LocalDateTime.now())
                 .name(organization.getName())
-                .owner(organization.getOwner()) // TODO [a.romanov]: swag fix
-                .metadata(objectMapper.writeValueAsString(organization.getMetadata()))
+                .owner(organization.getOwner())
+                .metadata(jsonMapper.toJson(organization.getMetadata()))
                 .build();
     }
 
@@ -37,6 +36,6 @@ public class OrganizationConverter {
                 .createdAt(OffsetDateTime.of(entity.getCreatedAt(), ZoneOffset.UTC))
                 .name(entity.getName())
                 .owner(entity.getOwner())
-                .metadata(objectMapper.readValue(entity.getMetadata(), Map.class));
+                .metadata(jsonMapper.toMap(entity.getMetadata()));
     }
 }
