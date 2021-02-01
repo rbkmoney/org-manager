7f68167ec6b4e94a6237ba220f2303155d0d0699
vitaxa <v.banin@rbkmoney.com>
2021-02-01 16:28:42 +0300

    Invitation status fix

--- src/main/java/com/rbkmoney/orgmanager/config/AppConfig.java
+++ src/main/java/com/rbkmoney/orgmanager/config/AppConfig.java
@@ -1,9 +1,12 @@
 package com.rbkmoney.orgmanager.config;
 
 import com.fasterxml.jackson.databind.Module;
+import com.rbkmoney.orgmanager.controller.converter.InvitationStatusConverter;
 import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.support.FormattingConversionService;
 
 @Configuration
 public class AppConfig {
@@ -11,6 +14,11 @@ public class AppConfig {
     @Bean
     public Module jsonNullableModule() {
         return new JsonNullableModule();
+    }
+
+    @Autowired
+    public void fillWebConverter(FormattingConversionService formatService) {
+        formatService.addConverter(new InvitationStatusConverter());
     }
 
 }
