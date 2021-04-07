78fe85db5ac021d09993b10b535afa7e9d5b2f5e
ggmaleva <griwania@gmail.com>
2021-04-07 16:18:51 +0300

    add null ignore property for jackson

--- src/main/java/com/rbkmoney/orgmanager/config/AppConfig.java
+++ src/main/java/com/rbkmoney/orgmanager/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.rbkmoney.orgmanager.config;
 
-import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rbkmoney.orgmanager.controller.converter.InvitationStatusConverter;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,11 @@ import org.springframework.format.support.FormattingConversionService;
 public class AppConfig {
 
     @Bean
-    public Module jsonNullableModule() {
-        return new JsonNullableModule();
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.registerModule(new JsonNullableModule());
+        return mapper;
     }
 
     @Autowired
