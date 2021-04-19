410e27441e37463b6729cfd62e26c8af7fcff9fe
ggmaleva <griwania@gmail.com>
2021-04-19 18:02:13 +0300

    fix review

--- src/main/java/com/rbkmoney/orgmanager/util/StackUtils.java
+++ src/main/java/com/rbkmoney/orgmanager/util/StackUtils.java
@@ -2,11 +2,9 @@ package com.rbkmoney.orgmanager.util;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
 
-@Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class StackUtils {
 
@@ -14,8 +12,7 @@ public class StackUtils {
         Optional<StackWalker.StackFrame> callerFrame = StackWalker.getInstance()
                 .walk(s -> s.skip(2).findFirst());
         if (callerFrame.isEmpty()) {
-            log.error("Can't get caller method name");
-            throw new RuntimeException();
+            throw new RuntimeException("Can't get caller method name");
         }
         return callerFrame.get().getMethodName();
     }
