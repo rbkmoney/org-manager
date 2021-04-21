8b43d52e84f925492a4374bdf5ef77167b809e97
ggmaleva <griwania@gmail.com>
2021-04-21 11:22:26 +0300

    refactoring test for user service + add log

--- src/main/java/com/rbkmoney/orgmanager/service/UserServiceImpl.java
+++ src/main/java/com/rbkmoney/orgmanager/service/UserServiceImpl.java
@@ -6,9 +6,11 @@ import com.rbkmoney.orgmanager.converter.MemberConverter;
 import com.rbkmoney.orgmanager.entity.MemberEntity;
 import com.rbkmoney.orgmanager.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -19,6 +21,7 @@ public class UserServiceImpl implements UserService {
     @Transactional(readOnly = true)
     @Override
     public User findById(String id) throws UserNotFound {
+        log.info("Find user with id {}", id);
         MemberEntity member = memberRepository.findById(id)
                 .orElseThrow(UserNotFound::new);
         return memberConverter.toThrift(member);
