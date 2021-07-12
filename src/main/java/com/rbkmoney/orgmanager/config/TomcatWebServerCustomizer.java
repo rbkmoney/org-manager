7bcdb492685fdd72d313e3302dc91eedff8b7e2e
Elena Kushchenko <kushchenko.elena@gmail.com>
2021-07-12 18:38:36 +0300

    JD-206 expose 8022 port

---
+++ src/main/java/com/rbkmoney/orgmanager/config/TomcatWebServerCustomizer.java
@@ -0,0 +1,22 @@
+package com.rbkmoney.orgmanager.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatWebServerCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    @Value("${server.thrift.port}")
+    private int thriftPort;
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        Connector connector = new Connector();
+        connector.setPort(thriftPort);
+
+        factory.addAdditionalTomcatConnectors(connector);
+    }
+}
